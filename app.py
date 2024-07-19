4bc3914123c860f2a23d340a20007f9165911a75
August Brunsman <august@lucidcarbon.com>
2024-07-19 15:27:42 -0400

    the first exercise

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-birth_year = input("Enter your birth year: ")
-age = 2024 - int(birth_year)
-print(age)
+weight = input("What is your weight: ")
+units = input ("(K)g or (L)bs? ")
+
+if units.upper() == "K":
+    ## do the stuff to convert kg to lbs
+    pounds = float(weight) * 2.2
+    print ("Your weight in pounds is " + str(pounds))
+    print ("right here")
+elif units.upper() == "L":
+    kilos = float(weight) / 2.2
+    print("Your weight in kilos: " + str(kilos))
+    ## do the convert lbs to kg
+else:
+    print("Incorrect unit choice.")
